af381b680abe3ec7475d5f84eca925fa5514318e
Vishal Sharma <vishalsharma28784@gmail.com>
2024-08-27 23:25:54 +0530

    bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
   try {
